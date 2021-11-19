58b90fa4529b6f4d0ab9abc879323bd09a21c3bb
farahmango <farahmangoo@gmail.com>
2021-11-19 16:15:43 +0300

    done

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $phones = [
     ],
     [ 
      'name' => 'INFINIX Zero 8',
-     'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001278_infinix-zero-8_220.jpeg',
+     'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001278_infinix-zero-8_490.jpeg',
     'rate' => '0',
     'brand' => 'Infinix',
     'price' => 'JOD 205.00',
@@ -27,7 +27,7 @@ $phones = [
     ],
     [ 
      'name' => 'ITEL A48',
-     'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001495_apple-iphone-12-pro_220.jpeg',
+     'img_url' =>'https://eshop.orange.jo/app-images/thumbs/0001532_itel-a48_220.jpeg',
     'rate' => '0',
     'brand' => 'iTel',
     'price' => 'JOD 66.00'
@@ -60,8 +60,12 @@ $phones = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mobile Phone | Orange Jordan E shop</title>
     <!-- Copyright © 2014 Monotype Imaging Inc. All rights reserved -->
-<link href="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/css/orange-helvetica.min.css" rel="stylesheet" integrity="sha384-ARRzqgHDBP0PQzxQoJtvyNn7Q8QQYr0XT+RXUFEPkQqkTB6gi43ZiL035dKWdkZe" crossorigin="anonymous">
-<link href="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/css/boosted.min.css" rel="stylesheet" integrity="sha384-Di/KMIVcO9Z2MJO3EsrZebWTNrgJTrzEDwAplhM5XnCFQ1aDhRNWrp6CWvVcn00c" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/css/orange-helvetica.min.css" rel="stylesheet" integrity="sha384-ARRzqgHDBP0PQzxQoJtvyNn7Q8QQYr0XT+RXUFEPkQqkTB6gi43ZiL035dKWdkZe" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/css/boosted.min.css" rel="stylesheet" integrity="sha384-Di/KMIVcO9Z2MJO3EsrZebWTNrgJTrzEDwAplhM5XnCFQ1aDhRNWrp6CWvVcn00c" crossorigin="anonymous">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -102,6 +106,36 @@ $phones = [
     </div>
   </div>
 </nav>
+  <div class="cont"> 
+<?php
+foreach($phones as $val){
+    echo "<div class='card card1' style='width: 25rem;height:52vh;'>";
+    echo " <img  class=' pic' src=" . $val["img_url"] . " />";
+    echo "<p class='card-text'>" . $val["brand"]   ."</p>";
+    echo " <h3 class='card-title'>" .  $val["name"]    ."</h3>";
+    echo "<span>";
+  if($val["rate"]==5){
+      for($i=0;$i<5; $i++){
+      echo " <i class='fa fa-star flix' aria-hidden='true' style='color:orange;'> " . "</i> ";
+        }
+  }
+  else{
+      for($i=0;$i<5; $i++){
+      echo " <i class='fa fa-star flix' aria-hidden='true' style='color:gray;'> " . "</i> ";
+    }  }
+    echo "<span class='card-text'>" ."(". $val["rate"] .")"  ."</span>";
+    echo "</span>";
+   //*********** price
+    echo "<hr>";
+    echo "<h4 class='price'>". $val["price"] ."</h4>"; 
+    echo "<span>";
+    echo "<a  class='btn btn-primary btn1'>" ."Buy Now " . " </a>";
+    echo "<a  class='btn btn-primary btn2'>" ."<i class='fa fa-cart-plus cart' aria-hidden='true'></i>" . " </a>";
+    echo "</span>";
+    echo "</div>";
+    }
+    ?>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/boosted@5.1.3/dist/js/boosted.bundle.min.js" integrity="sha384-5thbp4uNEqKgkl5m+rMBhqR+ZCs+3iAaLIghPWAgOv0VKvzGlYKR408MMbmCjmZF" crossorigin="anonymous"></script>
 </body>
 </html>
